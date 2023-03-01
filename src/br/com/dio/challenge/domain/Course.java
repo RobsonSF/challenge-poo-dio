cb5507ecf18f1537f8f7c4e5edd0139ee7c83ca2
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-01 13:24:30 -0300

    refactor Course and Mentoring

--- src/br/com/dio/challenge/domain/Course.java
+++ src/br/com/dio/challenge/domain/Course.java
@@ -1,58 +1,26 @@
 package br.com.dio.challenge.domain;
 
-public class Course {
-    private String title;
-    private String description;
+public class Course extends Content{
     private int workload;
-    public Course(String title, String description, int workload) {
-        this.title = title;
-        this.description = description;
-        this.workload = workload;
+
+    public Course() {
     }
-    public String getTitle() {
-        return title;
+ 
+    @Override
+    public double calcularXp() {
+        return XP_DEFAULT * workload;
     }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
+
     public int getWorkload() {
         return workload;
     }
+
     public void setWorkload(int workload) {
         this.workload = workload;
     }
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((title == null) ? 0 : title.hashCode());
-        return result;
-    }
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Course other = (Course) obj;
-        if (title == null) {
-            if (other.title != null)
-                return false;
-        } else if (!title.equals(other.title))
-            return false;
-        return true;
-    }
+
     @Override
     public String toString() {
-        return "Course [title=" + title + ", description=" + description + ", workload=" + workload + "]";
+        return "Course [title=" + getTitle() + ", description=" + getDescription() + ", workload=" + workload + "]";
     }
-    
 }
